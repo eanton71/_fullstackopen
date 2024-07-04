358e3aec4cd829b2412501eeddd96da04606fdd9
eanton71 <eanton71@gmail.com>
2024-07-04 18:43:36 +0200

    Exercice 2.3 courseinfo 8 reduce

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,14 +1,12 @@
 
 
 const Total = ({ parts }) => (
-     
-    
-    <strong>
-        total of {parts.reduce(
-      (acc, current) => acc + current.exercises,
-      0
-    )} exercices
-    </strong>
-)
+  <strong>
+    {/* CAlculamos la suma con reduce, se toma solo el campo exercices : current.exercices 
+    inicial = 0 en acc se guarda la suma acumulada*/}
+    total of {parts.reduce((acc, current) => acc + current.exercises, 0)}{" "}
+    exercices
+  </strong>
+);
 
 export default Total;

e779c7ab3ca5798b6f85f3f2fad8f60b20c73b87
eanton71 <eanton71@gmail.com>
2024-07-05 18:24:12 +0200

    exercice 2.7 phonebook 2 already added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,19 +3,32 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" ,id:1}]);
   const [newName, setNewName] = useState("");
+  /**
+   * añadir persona al array persons
+   * @param {*} event 
+   */
   const addPerson = (event) => {
     event.preventDefault();
-    const personObject = {
-      name: newName,
-      id: persons.length + 1,
-    };
+    //buscamos el nombre que esta en el input dentro del array persons para evitar ducplicados
+    //si esta mostramos un alert
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      //si no esta lo añadimos
+      const personObject = {
+        name: newName,
+        id: persons.length + 1,
+      };
 
-    setPersons(persons.concat(personObject));
-    setNewName("");
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
   };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      {/*formulario con el input para añadir peronas */}
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -29,6 +42,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      {/*renderizar  array persons mediante map */}
       {persons.map((person) => (
         <p key={person.id}>
            {person.name}

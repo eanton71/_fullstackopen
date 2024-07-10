9e3553f9b755058ac69b66bbe3c9a4c3e3ed7827
eanton71 <eanton71@gmail.com>
2024-07-10 18:55:53 +0200

    Exercice 2.15 * phonebook 10 update number

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
-import { useState, useEffect } from "react"; 
+import { useState, useEffect } from "react";
 import personService from "./services/persons";
 
 const App = () => {
@@ -15,16 +15,16 @@ const App = () => {
   //estado para input de busqueda
   const [filter, setFilter] = useState("");
 
-/**
- * llamada al servicio que se encarga de 
- * comunicarse con la base dedatos 
- * getAll: obtenemos toda la lista de peronsa
- * setPErsons: actuzalizar estado y  render app
- */
+  /**
+   * llamada al servicio que se encarga de
+   * comunicarse con la base dedatos
+   * getAll: obtenemos toda la lista de peronsa
+   * setPErsons: actuzalizar estado y  render app
+   */
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
       setPersons(initialPersons);
-      });
+    });
   }, []);
   console.log("render", persons.length, "persons");
 
@@ -34,48 +34,67 @@ const App = () => {
    */
   const addPerson = (event) => {
     event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
     //buscamos el nombre que esta en el input dentro del array persons para evitar ducplicados
-    //si esta mostramos un alert
-    if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    //si esta lo actualizamos
+    const personExists = persons.find((person) => person.name === newName);
+    if (personExists) {
+      // alert(`${newName} is already added to phonebook`);
+      if (window.confirm(`${newName} is in phonebook`)) {
+        personService
+          .update(personExists.id, personObject)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((p) =>
+                p.id !== personExists.id ? p : returnedPerson
+              )
+            );
+          })
+          .catch((error) => {
+            alert(
+              `Information of ${personExists.name} has already been removed from server`
+            );
+            setPersons(persons.filter((p) => p.id !== personExists.id));
+          });
+      }
     } else {
       //si no esta lo añadimos, no meter id
-      const personObject = {
-        name: newName,
-        number: newNumber 
-      };
-
-    personService.create(personObject).then((returnedPerson) => {
-      setPersons(persons.concat(returnedPerson));
-      setNewName("");
-      setNewNumber("");
-    });
-  
+      personService.create(personObject).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+      });
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+  const deletePerson = (id) => {
+    const person = persons.find((person) => person.id === id);
+    if (window.confirm(`${person.name} deleted`)) {
+      personService
+        .erase(person.id)
+        .then((retPerson) => {
+          console.log(`${retPerson.name} deleted`);
+        })
+        .catch((error) => alert(`Error: ${error.response.data.error}`));
+      setPersons(persons.filter((p) => p.id !== id));
     }
   };
-   const deletePerson = (id) => {
-     const person = persons.find((person) => person.id === id);
-     if (window.confirm(`${person.name} deleted`)) {
-       personService.erase(person.id).then((retPerson) => {
-         console.log(`${retPerson.name} deleted`);
-         setPersons(persons.filter((p) => p.id !== id));
-       });
-     }
-   };  
   /***
    * manejadores de eventos asociados al estado de los inputs
    */
   const handleChange = {
     name: (event) => {
-      setNewName(event.target.value);      
+      setNewName(event.target.value);
     },
     number: (event) => {
       setNewNumber(event.target.value);
     },
     filter: (event) => {
       setFilter(event.target.value);
-      },
-  }
+    },
+  };
 
   return (
     <div>
@@ -97,11 +116,7 @@ const App = () => {
       <h2>Numbers</h2>
       {/*renderizar  array persons mediante map , prevismante hace un filtrado
       para que solo muestre los que coinciden con el input asociado al estado filter*/}
-      <Persons
-        persons={persons}
-        filter={filter}
-        deletePerson={deletePerson}
-      />
+      <Persons persons={persons} filter={filter} deletePerson={deletePerson} />
     </div>
   );
 };
